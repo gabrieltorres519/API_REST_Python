de603702d3cce113f8fa5de1e3f916bb006634c8
gabrieltorres519 <gabrieltorres9909@gmail.com>
2022-06-21 13:13:58 -0500

    Visualizando el comportamiento de los métodos disparar() usando polimorfismo

--- POO/poo5.py
+++ POO/poo5.py
@@ -15,9 +15,19 @@ class Pistola(Arma):
     def disparar(self):
         print("El arma dispara lento");
 
+class Ametralladora(Arma):
+
+    def disparar(self):
+        print("El arma dispara rápido");
+
 
 
 arma = Arma(15,7)
 arma.disparar()
+
 pistola = Pistola(12, 5)
 pistola.disparar()
+
+ametralladora = Ametralladora(50, 12)
+ametralladora.disparar()
+
