be9d75758a60624ef9281ab01a342beba5640dae
gabrieltorres519 <gabrieltorres9909@gmail.com>
2022-06-21 11:38:34 -0500

    Creando una clase catálogo y una pelicula y agregando la pelicula al listado del catálogo, note que imprime la dirección de memoria de la lista de peliculas (array) pero no el contenido, pues por alguna razón no hace cas al método __str__

---
+++ POO/poo3.py
@@ -0,0 +1,23 @@
+class Catalogo:
+
+    def __init__(self, nombre):
+        self.nombre = nombre
+        self.peliculas = []
+
+class Pelicula:
+
+    def __init__(self, nombre, duracion, genero):
+        self.nombre = nombre
+        self.duracion = duracion #minutos
+        self.genero = genero
+
+    def __str__(self):
+        return f"{self.nombre}, {self.duracion}, {self.genero}"
+
+catalogo1 = Catalogo("Catalogo de terror");
+pelicula = Pelicula("Actividad paranormal", 120, "terror");
+
+print(catalogo1.peliculas);
+catalogo1.peliculas.append(pelicula);
+print(catalogo1.peliculas);
+
