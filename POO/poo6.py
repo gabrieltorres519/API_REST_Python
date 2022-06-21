65ba74d53dabc1923c667e5d22431b7629b5e66f
gabrieltorres519 <gabrieltorres9909@gmail.com>
2022-06-21 13:40:11 -0500

    Usando setters para modificar atributos privados (encapsulados) de la clase

--- POO/poo6.py
+++ POO/poo6.py
@@ -8,13 +8,17 @@ class Curso:
     def __adquirir_curso(self):
         print("Has adquirido este curso");
 
-    # Simulando setters y getters para trabajar con atributos y metodos privados (encapsulados)
+    # Simulando getters para trabajar con atributos y metodos privados (encapsulados)
     def get_adquirir_curso(self):
         return self.__adquirir_curso();
     
     def get_titulo(self):
         return self.__titulo;
     
+    # Simulando setters para trabajar con atributos y metodos privados (encapsulados)
+    def set_titulo(self, titulo):
+        self.__titulo = titulo;
+
 
 
 curso = Curso()
@@ -23,4 +27,6 @@ curso = Curso()
 # curso.__adquirir_curso() # No se podrá acceder porque es privado
 
 curso.get_adquirir_curso();
-print(curso.get_titulo);
+print(curso.get_titulo());
+curso.set_titulo("Backend Python PRO");
+print(curso.get_titulo());
