1a69de750ce23ee55980a3535fb2b385b096c1b3
gabrieltorres519 <gabrieltorres9909@gmail.com>
2022-06-21 14:09:51 -0500

    Importando una clase específica en el archivo

--- BasicExtra/programa2.py
+++ BasicExtra/programa2.py
@@ -1,7 +1,15 @@
-import programa1 
+# import programa1 
 
-print(programa1.saludar(programa1.nombre));
+# print(programa1.saludar(programa1.nombre));
 
-user = programa1.Usuario("Mario", "mario@gmail.com");
+# user = programa1.Usuario("Mario", "mario@gmail.com");
+# print(user.nombre);
+# print(user.email);
+
+#Importación de recursos específicos 
+
+from programa1 import Usuario;
+
+user = Usuario("Mario", "mario@gmail.com");
 print(user.nombre);
 print(user.email);
