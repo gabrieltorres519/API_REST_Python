586b0ee402f8ab30c5aaf08602b564fc247e52f6
gabrieltorres519 <gabrieltorres9909@gmail.com>
2022-06-20 20:08:06 -0500

    Usando el método __str__ para imprimir todos los atributos al hacer print del objeto instanciado

--- BasicExercises/poo.py
+++ BasicExercises/poo.py
@@ -12,10 +12,11 @@ class Auto:
       de sus atributos.
     """
     def __str__(self):
-        return f"{self.motor}";
+        return f"Auto: {self.motor}, {self.marca}, {self.cantidad_ruedas}, {self.velocidad} \n";
 
 aventador = Auto("Lamborgini","blanco",4,320);
 
 print(aventador); # Se imprime la dirección en memoria en la que se está almacenando el objeto Auto Aventador
-print(aventador.motor); # Se imprimirá dos veces el motor si tenemos el Método __str__ sin comentar
+                  # Después de agregar el método __str__ ya no se imprime la dirección en memoria, sino solo __str__
+#print(aventador.motor); # Se imprimirá dos veces el motor si tenemos el Método __str__ sin comentar
 
