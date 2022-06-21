b022ba81e46973bc1093a1f31848d02de53363d2
gabrieltorres519 <gabrieltorres9909@gmail.com>
2022-06-21 13:53:39 -0500

    Visualizando el error generado en try except

--- BasicExtra/manejoerrores.py
+++ BasicExtra/manejoerrores.py
@@ -1,5 +1,5 @@
 try:
     menu = int(input("Ingrese un numero entero: "));
     print(f"Ha ingresado la opcion {menu}");
-except ValueError:
-    print(f"No puede ingresar ese tipo de dato");
+except ValueError as error:
+    print(f"No puede ingresar ese tipo de dato, el error es: {error}");
